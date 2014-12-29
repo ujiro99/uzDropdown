ad2a643d531fca6a751322ee9f18d471a5a4a5a7
ujiro99 <siro.cola@gmail.com>
2014-12-30 07:36:04 +0900

    release build

--- dist/scripts/uz-dropdown.js
+++ dist/scripts/uz-dropdown.js
@@ -1,101 +1,138 @@
-angular.module('uz', []).directive('uzDropdown', [
-  '$timeout',
-  function ($timeout) {
-    return {
-      restrict: 'E',
-      template: '<div class=\'dropdown\'>' + '<input tabindex=\'0\'' + 'class=\'dropdown-text dropdown-toggle\'' + 'ng-model=\'keyword\'' + 'placeholder=\'{{placeholder}}\'' + 'ng-keydown=\'onKeydown($event.keyCode)\'></input>' + '<div class=\'dropdown-box\'><ul class=\'dropdown-content\'>' + '<li ng-repeat=\'item in result = (items | filter:itemFilter)\'' + 'ng-click=\'onClickItem(item)\'' + 'ng-class=\'{active: item === selected[0]}\'>' + '<a><span>{{$eval($parent.format)}}</span></a>' + '</li></ul></div>',
-      scope: {
-        items: '=',
-        selected: '=',
-        placeholder: '@'
-      },
-      link: function (scope, element, attrs) {
-        var KEY_DOWN, KEY_ENTER, KEY_UP, SPLIT_SPACE, escapeRegExp, _searchRegex, _selectIndex;
-        KEY_ENTER = 13;
-        KEY_UP = 38;
-        KEY_DOWN = 40;
-        SPLIT_SPACE = ' ';
-        _selectIndex = 0;
-        _searchRegex = {};
-        escapeRegExp = function (str) {
-          return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-        };
-        scope.keyword = '';
-        scope.result = [];
-        scope.format = attrs.format || 'item.text';
-        scope.selectedFormat = attrs.selectedFormat || 'selected[0].text';
-        /*
-       filtering list
-      */
-        scope.itemFilter = function (item) {
-          var isMatch, k, keywords, reg, target, _i, _j, _len, _len1, _ref;
-          if (!_searchRegex[scope.keyword]) {
-            _searchRegex = {};
-            _searchRegex[scope.keyword] = [];
-            keywords = scope.keyword.split(SPLIT_SPACE);
-            for (_i = 0, _len = keywords.length; _i < _len; _i++) {
-              k = keywords[_i];
-              _searchRegex[scope.keyword].push(new RegExp(escapeRegExp(k), ['i']));
+angular.module('uz', []).directive('uzDropdown', function () {
+  return {
+    restrict: 'E',
+    template: '<div class=\'dropdown\'>' + '<input tabindex=\'0\'' + 'class=\'dropdown-text dropdown-toggle\'' + 'ng-model=\'keyword\'' + 'placeholder=\'{{placeholder}}\'' + 'ng-blur=\'onBlur()\'' + 'ng-keydown=\'onKeydown($event.keyCode)\'></input>' + '<div class=\'dropdown-box\'><ul class=\'dropdown-content\'>' + '<li ng-repeat=\'item in result = itemFilter(items)\'' + 'ng-click=\'onClickItem(item)\'' + 'ng-class=\'{active: item === selected[0]}\'>' + '<a><span>{{$format(format, item)}}</span></a></li>' + '<li ng-if=\'result.length == 0\'><a><span>not found ...</span></a></li>' + '<li ng-if=\'result.length >= listMax\' ng-mouseover=\'onMouseover()\'>' + '<a><span>more ...</span></a></li>' + '</ul></div>',
+    scope: {
+      items: '=',
+      selected: '=',
+      placeholder: '@'
+    },
+    link: function (scope, element, attrs) {
+      var DEFAULT_FORMAT, KEY_DOWN, KEY_ENTER, KEY_UP, LIST_MAX_INITIAL, SPLIT_SPACE, escapeRegExp, _selectIndex;
+      KEY_ENTER = 13;
+      KEY_UP = 38;
+      KEY_DOWN = 40;
+      LIST_MAX_INITIAL = 100;
+      SPLIT_SPACE = ' ';
+      DEFAULT_FORMAT = '{0}';
+      _selectIndex = 0;
+      scope.keyword = '';
+      scope.result = [];
+      scope.format = attrs.format || DEFAULT_FORMAT;
+      scope.listMax = LIST_MAX_INITIAL;
+      escapeRegExp = function (str) {
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+      };
+      scope.$format = function (fmtStr, obj) {
+        var args, rep_fn;
+        rep_fn = void 0;
+        if (typeof obj === 'object') {
+          rep_fn = function (m, k) {
+            var key, keys, tmp, _i, _len;
+            keys = k.split('.');
+            tmp = obj;
+            for (_i = 0, _len = keys.length; _i < _len; _i++) {
+              key = keys[_i];
+              tmp = tmp[key];
             }
-          }
-          target = eval(scope.format);
+            return tmp;
+          };
+        } else {
+          args = arguments;
+          rep_fn = function (m, k) {
+            return args[parseInt(k) + 1];
+          };
+        }
+        return fmtStr.replace(/\{(.+?)\}/g, rep_fn);
+      };
+      /*
+       filtering list.
+      */
+      scope.itemFilter = function (items) {
+        var end, filteredItems, i, isMatch, item, k, keywords, reg, regs, target, _i, _j, _k, _len, _len1, _len2;
+        if (!scope.keyword) {
+          end = items.length < scope.listMax ? items.length : scope.listMax;
+          return items.slice(0, end);
+        }
+        regs = [];
+        keywords = scope.keyword.split(SPLIT_SPACE);
+        for (_i = 0, _len = keywords.length; _i < _len; _i++) {
+          k = keywords[_i];
+          regs.push(new RegExp(escapeRegExp(k), ['i']));
+        }
+        filteredItems = [];
+        for (i = _j = 0, _len1 = items.length; _j < _len1; i = ++_j) {
+          item = items[i];
+          target = scope.$format(scope.format, item);
           isMatch = true;
-          _ref = _searchRegex[scope.keyword];
-          for (_j = 0, _len1 = _ref.length; _j < _len1; _j++) {
-            reg = _ref[_j];
+          for (_k = 0, _len2 = regs.length; _k < _len2; _k++) {
+            reg = regs[_k];
             isMatch &= reg.test(target);
           }
-          return isMatch;
-        };
-        /*
-       on focus input, select all words.
+          if (isMatch && filteredItems.length < scope.listMax) {
+            filteredItems.push(item);
+          }
+        }
+        return filteredItems;
+      };
+      /*
+       on focus to input, select all words.
       */
-        element[0].querySelector('input').onfocus = function () {
-          return this.select();
-        };
-        /*
+      element[0].querySelector('input').onfocus = function () {
+        return this.select();
+      };
+      /*
+       on blur from input, reset state.
+      */
+      scope.onBlur = function () {
+        return scope.listMax = LIST_MAX_INITIAL;
+      };
+      /*
        on click contents, select item and update input field.
       */
-        scope.onClickItem = function (item) {
-          scope.selected[0] = item;
-          return scope.keyword = eval('scope.' + scope.selectedFormat);
-        };
-        /*
+      scope.onClickItem = function (item) {
+        scope.selected[0] = item;
+        return scope.keyword = scope.$format(scope.format, item);
+      };
+      /*
        Keydown event on input fileld.
         - select item (up and down).
         - confirm item selection (enter).
       */
-        scope.onKeydown = function (keycode) {
-          var tmpSelected;
-          if (keycode === KEY_ENTER) {
-            scope.keyword = eval('scope.' + scope.selectedFormat);
-            tmpSelected = scope.result[_selectIndex];
-          } else if (keycode === KEY_DOWN) {
-            _selectIndex++;
-          } else if (keycode === KEY_UP) {
-            _selectIndex--;
-          }
-          if (_selectIndex < 0) {
-            _selectIndex = 0;
-          }
-          if (_selectIndex >= scope.result.length) {
-            _selectIndex = scope.result.length - 1;
-          }
+      scope.onKeydown = function (keycode) {
+        var tmpSelected;
+        if (keycode === KEY_ENTER) {
           tmpSelected = scope.result[_selectIndex];
-          return $timeout(function () {
-            return scope.selected[0] = tmpSelected;
-          });
-        };
-        /*
+          if (tmpSelected) {
+            scope.keyword = scope.$format(scope.format, tmpSelected);
+          }
+        } else if (keycode === KEY_DOWN) {
+          _selectIndex++;
+        } else if (keycode === KEY_UP) {
+          _selectIndex--;
+        }
+        if (_selectIndex < 0) {
+          _selectIndex = 0;
+        }
+        if (_selectIndex >= scope.result.length) {
+          _selectIndex = scope.result.length - 1;
+        }
+        tmpSelected = scope.result[_selectIndex];
+        return scope.selected[0] = tmpSelected;
+      };
+      /*
+       on mouseover last item, load more items.
+      */
+      scope.onMouseover = function () {
+        return scope.listMax += 100;
+      };
+      /*
        Update selection if result changed.
       */
-        return scope.$watch('result.length', function () {
-          _selectIndex = 0;
-          return $timeout(function () {
-            return scope.selected[0] = scope.result[_selectIndex];
-          });
-        });
-      }
-    };
-  }
-]);
+      return scope.$watch('result.length', function () {
+        _selectIndex = 0;
+        return scope.selected[0] = scope.result[_selectIndex];
+      });
+    }
+  };
+});
